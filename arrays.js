145f86aeaa57e0607c184769d3ce4120df572ff5
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-09 21:57:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,4 +18,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {[array.shift()];
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)}
   
-function
+function destructivelyRemoveElementFromBeginningOfArray(array) { array.shift(); return array}
+
+function destructivelyRemoveElementFromEndOfArray(array) { array.pop(); return array}
+
+function removeElementFromEndOfArray(array) {return array.slice(0, array.length -1)}
