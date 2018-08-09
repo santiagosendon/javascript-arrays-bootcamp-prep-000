0fee242a4d5473e6994cf7696d490bf85d2a3c8d
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-09 20:56:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,4 +3,4 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element)
 {return [element, ...array]}
 
-function destuctivelyAddElementToBeginningOfArray(array, element) {return []
+function destuctivelyAddElementToBeginningOfArray(array, element) {return [element, array]}
