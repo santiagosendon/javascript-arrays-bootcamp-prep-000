a02cc723559a12a188113e0a07d3c8dc6e409f27
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-09 21:02:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,5 @@ function addElementToBeginningOfArray(array, element)
 {return [element, ...array]}
 
 function destructivelyAddElementToBeginningOfArray(array, element) {[array.unshift(element)]; return array}
+
+function addElementToEndOfArray(array, element) {[array.push(element)]; return array}
