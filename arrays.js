00e4914cd1825e319e7e8a098517c4fefac60ffe
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-09 21:14:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,5 @@ function addElementToEndOfArray(array, element) {return [...array, element]}
 function destructivelyAddElementToEndOfArray(array, element) {[array.push(element)]; return array}
 
 function accessElementInArray(array, index) {return array[index]}
+
+function des
